8b991e401e8412680a7fe0d9b47a7dc304bc51b9
Zahharov,Sven S <zahharov@hotmail.com>
2017-10-04 10:26:26 +0200

    Added running method to AEXBanner domain

--- src/sample/Domain/AEXBanner.java
+++ src/sample/Domain/AEXBanner.java
@@ -6,9 +6,24 @@ public class AEXBanner extends Application {
 	 * 
 	 * @param koersen
 	 */
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+		primaryStage.setTitle("AEXBanner");
+		primaryStage.setScene(new Scene(root, 300, 275));
+		primaryStage.show();
+	}
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 	public void setKoersen(string koersen) {
 		// TODO - implement AEXBanner.setKoersen
 		throw new UnsupportedOperationException();
 	}
+
 
 }
